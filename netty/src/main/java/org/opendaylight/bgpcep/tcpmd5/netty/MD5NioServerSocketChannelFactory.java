opendaylight/bgpcep

BUG-634: add tcpmd5 configuration binding
Change-Id: I64d400fb7083a5e9de8a88d067d4d16358fabfff
Signed-off-by: Robert Varga <rovarga@cisco.com>

tcp-md5/netty/src/main/java/org/opendaylight/bgpcep/tcpmd5/netty/MD5NioServerSocketChannelFactory.java
@@ -7,13 +7,11 @@
  */
 package org.opendaylight.bgpcep.tcpmd5.netty;
 
-import io.netty.bootstrap.ChannelFactory;
-
 import org.opendaylight.bgpcep.tcpmd5.KeyAccessFactory;
 
 import com.google.common.base.Preconditions;
 
-public final class MD5NioServerSocketChannelFactory implements ChannelFactory<MD5NioServerSocketChannel> {
+public class MD5NioServerSocketChannelFactory implements MD5ChannelFactory<MD5NioServerSocketChannel> {
 	private final KeyAccessFactory keyAccessFactory;
 
 	public MD5NioServerSocketChannelFactory(final KeyAccessFactory keyAccessFactory) {

opendaylight/archived-tcpmd5

netty/src/main/java/org/opendaylight/bgpcep/tcpmd5/netty/MD5NioServerSocketChannelFactory.java
@@ -7,13 +7,11 @@
  */
 package org.opendaylight.bgpcep.tcpmd5.netty;
 
-import io.netty.bootstrap.ChannelFactory;
-
 import org.opendaylight.bgpcep.tcpmd5.KeyAccessFactory;
 
 import com.google.common.base.Preconditions;
 
-public final class MD5NioServerSocketChannelFactory implements ChannelFactory<MD5NioServerSocketChannel> {
+public class MD5NioServerSocketChannelFactory implements MD5ServerChannelFactory<MD5NioServerSocketChannel> {
 	private final KeyAccessFactory keyAccessFactory;
 
 	public MD5NioServerSocketChannelFactory(final KeyAccessFactory keyAccessFactory) {